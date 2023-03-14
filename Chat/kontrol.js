6a3367c823705196725fc804f88a77abc081caf5
erhanyagmur <119954665+erhanyagmur@users.noreply.github.com>
2023-03-14 23:21:39 +0300

    Add files via upload

---
+++ Chat/kontrol.js
@@ -0,0 +1,13 @@
+function mesajGonder()
+{
+	var mesaj = document.getElementById("mesaj").value;
+	var d=new Date();
+	var saat = d.getHours();
+	var dakika = d.getMinutes();
+	
+	document.getElementById("mesajKutulari").innerHTML+=
+		"<div class='gidenMesaj'> <img src='erhan.jpg'> "
+		+mesaj+
+		"<span id='tarih'>"+saat+":"+dakika+"</span></div>";
+	document.getElementById("mesaj").value="";
+}
